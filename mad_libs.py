747b7af662d51debd8103c80e08b94395fb32286
cecwabuti <43781850+cecilwabuti@users.noreply.github.com>
2020-12-28 02:00:18 +0300

    Add files via upload
    These are games developed that contain the basics of python. I try to explore  string object types ,  variables, strings, expressions,branching and conditions, indentation and loops.
    I have used Spyder as my IDE(its amazing by the way).

---
+++ mad_libs.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 12 17:09:47 2020
+
+@author: CEC
+"""
+
+# variables
+#strings
+#input
+#print
+
+
+word_1 = input()
+word_2 = input()
+word_3 = input()
+word_4 = input()
+print(word_1, word_2, word_3, word_4)
